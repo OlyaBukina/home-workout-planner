3e16c57ba1af27c91c3640fbb6e8d34f651b53d2
Olya <katniss30051991@gmail.com>
2024-02-05 19:11:15 +0200

    Created home page and start creating login page

---
+++ src/components/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Rowdies } from "next/font/google";
+import { Urbanist } from "next/font/google";
+
+export const rowdies = Rowdies({ subsets: ["latin"], weight: ["400"] });
+export const urbanist = Urbanist({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
